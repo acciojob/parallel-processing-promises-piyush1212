4e2b6cca975df82c001f8d70fa1da1a10f59a3f4
Piyush Kumar Yadav <73697893+piyush1212@users.noreply.github.com>
2024-04-20 20:34:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
+document.addEventListener("DOMContentLoaded", function() {
+  const output = document.getElementById("output");
+  const btn = document.getElementById("download-images-button");
 
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
+  const images = [
+    { url: "https://picsum.photos/id/237/200/300" },
+    { url: "https://picsum.photos/id/238/200/300" },
+    { url: "https://picsum.photos/id/239/200/300" },
+  ];
 
-function downloadAndDisplayImages(imageArray) {
-  const promises = imageArray.map(image => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.onload = () => {
-        resolve(img);
-      };
-      img.onerror = () => {
-        reject(new Error(`Failed to load image's URL: ${image.url}`));
-      };
-      img.src = image.url;
-    });
-  });
-
-  Promise.all(promises)
-    .then(images => {
-      images.forEach(img => {
-        output.appendChild(img);
+  function downloadAndDisplayImages(imageArray) {
+    const promises = imageArray.map(image => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => {
+          resolve(img);
+        };
+        img.onerror = () => {
+          reject(new Error(`Failed to load image's URL: ${image.url}`));
+        };
+        img.src = image.url;
       });
-    })
-    .catch(error => {
-      console.error(error);
     });
-}
 
-btn.addEventListener('click', () => {
-  downloadAndDisplayImages(images);
+    Promise.all(promises)
+      .then(images => {
+        images.forEach(img => {
+          output.appendChild(img);
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  btn.addEventListener('click', () => {
+    downloadAndDisplayImages(images);
+  });
 });
